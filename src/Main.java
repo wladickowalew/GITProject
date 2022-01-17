2aedac9d475786b66306a3c8d1965ea88f80a838
kowlad <wladickowalew@gmail.com>
2022-01-17 16:47:44 +0300

    GitHub commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello Git!");
         System.out.println("For Second commit");
+        System.out.println("HelloGitHub");
     }
 }
